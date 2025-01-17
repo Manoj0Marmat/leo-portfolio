27902de4ddc1dec5d9a892880f202e3542fc366a
Leo <manojrajput762000@gmail.com>
2025-01-18 00:18:18 +0530

    feat(projects): added ui for projects section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: false,
-  
+
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  range: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
