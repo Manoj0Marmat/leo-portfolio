a80a5a5151fb9ef6684933e4818df17334320550
Leo <manojrajput762000@gmail.com>
2025-01-18 00:17:32 +0530

    refactor(profile): removed hardcoded button and added dynamically generation

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   standalone: false,
-  
+
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
-
+  cRoutes = [
+    { text: 'About Me', link: '/about-me' },
+    { text: 'Skills', link: '/skills' },
+    { text: 'Projects', link: '/projects' },
+    { text: 'Certificates', link: '/certificates' },
+  ];
 }
