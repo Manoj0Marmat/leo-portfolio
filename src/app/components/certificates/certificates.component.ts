053ed383b00e41f80ad59c837878bca14f90398f
Leo <manojrajput762000@gmail.com>
2025-01-18 16:08:26 +0530

    feat(certificate): complete certificate component and animations

--- src/app/components/certificates/certificates.component.ts
+++ src/app/components/certificates/certificates.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-certificates',
   standalone: false,
-  
   templateUrl: './certificates.component.html',
-  styleUrl: './certificates.component.scss'
+  styleUrls: ['./certificates.component.scss'],
 })
 export class CertificatesComponent {
+  // List of images (can be fetched from an API or defined statically)
+  images = [
+    {
+      src: 'https://udemy-certificate.s3.amazonaws.com/image/UC-a58d1fbc-4b18-4cc9-8061-d40a2ae30e1a.jpg?v=1736330374000',
+      alt: "Asp.Net Core : The Complete Guide To Build RESTful Api's",
+    },
+    {
+      src: 'https://udemy-certificate.s3.amazonaws.com/image/UC-ad9276d4-1e95-411b-a500-3e12e0bf8067.jpg?v=1735233680000',
+      alt: '.NET Core MVC',
+    },
+  ];
 
+  constructor(private router: Router) {}
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
 }
